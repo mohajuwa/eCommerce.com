07828b7f942ffec992741e2509fe6c8e9ff6c2e8
mohajuwa <mohaghram2019@gmail.com>
2024-04-18 22:07:51 +0300

    secondOne

--- resources/views/admin/brand/list.blade.php
+++ resources/views/admin/brand/list.blade.php
@@ -29,7 +29,8 @@
 
                     <!-- /.card-header -->
                     <div class="card-body p-0   ">
-                        <table class="table table-striped table-responsive-lg  table-responsive-sm table-responsive-md table-responsive-xxl">
+                        <table
+                            class="table table-striped table-responsive-lg  table-responsive-sm table-responsive-md table-responsive-xxl">
                             <thead>
                                 <tr>
                                     <th>#</th>
@@ -49,21 +50,21 @@
                                     <td>{{ $value->id }}</td>
                                     <td>{{ $value->name }}</td>
                                     <td>{{ $value->slug }}</td>
-                                    <td>{{ $value->created_by_name }}</td>
+                                    <td><span class="badge bg-secondary">{{ $value->created_by_name }}</span>
+                                    </td>
 
 
+                                    <td><span class="badge bg-{{ $value->status == 0 ? 'info' : 'danger' }}">{{
+                                            $value->status == 0 ? 'Active' : 'Inactive' }}</span>
+                                    </td>
+                                    <td>{{ date('d-m-y', strtotime($value->created_at)) }}</td>
 
-                                        <td><span class="badge bg-{{ $value->status == 0 ? 'info' : 'danger' }}">{{
-                                                $value->status == 0 ? 'Active' : 'Inactive' }}</span>
-                                        </td>
-                                        <td>{{ date('d-m-y', strtotime($value->created_at)) }}</td>
-
-                                        <td>
-                                            <a href="{{ url('admin/brand/edit/' . $value->id) }}"
-                                                class="btn btn-primary">Edit</a>
-                                            <a href="{{ url('admin/brand/delete/' . $value->id) }}"
-                                                class="btn btn-danger">delete</a>
-                                        </td>
+                                    <td>
+                                        <a href="{{ url('admin/brand/edit/' . $value->id) }}"
+                                            class="btn btn-primary">Edit</a>
+                                        <a href="{{ url('admin/brand/delete/' . $value->id) }}"
+                                            class="btn btn-danger">delete</a>
+                                    </td>
                                 </tr>
                                 @endforeach
 
